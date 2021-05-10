36659d349670601af0330c69eb9283582fb3caff
David Rosenberg <dmr@davidrosenberg.me>
2021-05-10 18:46:56 -0400

    restructure

---
+++ cmd/drchive/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "github.com/rosenbergdm/dRchive/internal"
+  "github.com/rosenbergdm/dRchive/internal/db"
+)
+
+func main() {
+  fmt.Println("Hello, world")
+}
+
