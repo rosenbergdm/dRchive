2f35610cbcbe3fc093aa9d9a248467f90bf34b53
David Rosenberg <dmr@davidrosenberg.me>
2021-05-17 19:59:17 -0400

    cmd line parsing

--- cmd/drchive/main.go
+++ cmd/drchive/main.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"fmt"
-	"github.com/rosenbergdm/dRchive/internal/db"
 	"os"
+
+	docopt "github.com/docopt/docopt-go"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("USAGE: ", os.Args[0], " <DATABASE>")
-		os.Exit(1)
+	usage := `Usage:
+  drchive [--debug] --config=<configdir> --source=<srcdir> --target=<targetdir> --image=<imagedir>
+  drchive [--debug] --configfile=<configfile>
+  drchive [-h | --help]
+  drchive
+
+Run the dRchive program
+
+Options:
+  -h --help                   Display usage
+  --debug                     Set maximal output
+  -v --verbose                Increase verbosity
+  --config=<configfile>       Directory containing configuration components
+  --source=<srcdir>           Directory to read from
+  --target=<targetdir>        Directory to archive into
+  --image=<imagedir>          Image directory
+  --configfile=<configfile>   File containing all configuration params`
+
+	opts, err := docopt.ParseArgs(usage, os.Args[1:], "0.0.1")
+
+	if err == nil {
+		fmt.Println("Arguments parsed successfully")
+		fmt.Println(opts)
+	} else {
+		fmt.Printf("%+v\n", err)
+		fmt.Println("Arguments NOT parsed successfully")
+		fmt.Println(err.Error())
 	}
-	dbname := os.Args[1]
-	db, err := db.CreateDb(dbname)
-	if err != nil {
-		fmt.Println("Error creating DB!  Aborting")
-		os.Exit(99)
-	}
-	db.Close()
-	fmt.Println("Success")
+
+	os.Exit(0)
+	// 	if len(os.Args) < 2 {
+	// 		fmt.Println("USAGE: ", os.Args[0], " <DATABASE>")
+	// 		os.Exit(1)
+	// 	}
+	// 	dbname := os.Args[1]
+	// 	db, err := db.CreateDb(dbname)
+	// 	if err != nil {
+	// 		fmt.Println("Error creating DB!  Aborting")
+	// 		os.Exit(99)
+	// 	}
+	// 	db.Close()
+	// 	fmt.Println("Success")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rosenbergdm/dRchive
 go 1.16
 
 require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	github.com/motemen/gore v0.5.2 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
