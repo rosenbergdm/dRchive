2877892bc426bdffa5029d2a5b9a81c5ad843121
David Rosenberg <dmr@davidrosenberg.me>
2021-05-17 21:14:08 -0400

    cmdline parsing

--- cmd/drchive/main.go
+++ cmd/drchive/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+  "flag"
 
-	docopt "github.com/docopt/docopt-go"
+	// docopt "github.com/docopt/docopt-go"
 )
 
 func main() {
@@ -17,27 +18,44 @@ func main() {
 Run the dRchive program
 
 Options:
-  -h --help                   Display usage
+  --help                      Display usage
   --debug                     Set maximal output
-  -v --verbose                Increase verbosity
-  --config=<configfile>       Directory containing configuration components
+  --verbose                   Increase verbosity
+  --config=<configdir>        Directory containing configuration components
   --source=<srcdir>           Directory to read from
   --target=<targetdir>        Directory to archive into
   --image=<imagedir>          Image directory
-  --configfile=<configfile>   File containing all configuration params`
+  --configfile=<configfile>   File containing all configuration params
+  --version                   Version
+`
+  help := flag.Bool("help", false, "Display usage")
+  debug := flag.Bool("debug", false, "Set maximal output")
+  verbose := flag.Bool("verbose", false, "Set verbose output")
+  version := flag.Bool("version", false, "Display version number")
+  configfile := flag.String("configfile", "", "Configuration file")
 
-	opts, err := docopt.ParseArgs(usage, os.Args[1:], "0.0.1")
+  flag.Parse()
+  fmt.Println("'help' has value: ", *help)
+  fmt.Println("'debug' has value: ", *debug)
+  fmt.Println("'verbose' has value: ", *verbose)
+  fmt.Println("'version' has value: ", *version)
+  fmt.Println("'configfile' has value: ", *configfile)
 
-	if err == nil {
-		fmt.Println("Arguments parsed successfully")
-		fmt.Println(opts)
-	} else {
-		fmt.Printf("%+v\n", err)
-		fmt.Println("Arguments NOT parsed successfully")
-		fmt.Println(err.Error())
-	}
+  fmt.Println(usage)
 
-	os.Exit(0)
+  os.Exit(0)
+
+
+// 	opts, err := docopt.ParseArgs(usage, os.Args[1:], "0.0.1")
+
+// 	if err == nil {
+// 		fmt.Println("Arguments parsed successfully")
+// 		fmt.Println(opts)
+// 	} else {
+// 		fmt.Printf("%+v\n", err)
+// 		fmt.Println("Arguments NOT parsed successfully")
+// 		fmt.Println(err.Error())
+// 	}
 	// 	if len(os.Args) < 2 {
 	// 		fmt.Println("USAGE: ", os.Args[0], " <DATABASE>")
 	// 		os.Exit(1)
