31b81d95047573c3c3c9d4625c94c1bd5612e1b8
David Rosenberg <dmr@davidrosenberg.me>
2021-05-19 06:15:58 -0400

    better cli parsing

--- cmd/drchive/main.go
+++ cmd/drchive/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-	// "flag"
-	"fmt"
 	"os"
 
 	docopt "github.com/docopt/docopt-go"
+
+	"github.com/rosenbergdm/dRchive/internal/log"
 )
+
+func init() {
+	log.ConfigLogger(log.WarnLevel, os.Stdout)
+}
 
 func main() {
 	usage := `Usage:
-  drchive [--debug] --config=<configdir> --source=<srcdir> --target=<targetdir> --image=<imagedir>
-  drchive [--debug] --configfile=<configfile>
+  drchive [-vhdq] [--debug] --config=<configdir> --source=<srcdir> --target=<targetdir> --image=<imagedir>
+  drchive [-vhdq] [--debug] --configfile=<configfile>
   drchive [-h | --help]
   drchive
 
 Run the dRchive program
 
 Options:
-  --help                      Display usage
+  --help -h                   Display usage
   --debug                     Set maximal output
   --verbose                   Increase verbosity
+  --quiet -q                  Suppress output
   --config=<configdir>        Directory containing configuration components
   --source=<srcdir>           Directory to read from
   --target=<targetdir>        Directory to archive into
@@ -47,21 +52,17 @@ Options:
 	// log.LogError(logrus.Fields{}, "help************************")
 	// log.LogWarn(logrus.Fields{"testing": "123"}, "help")
 	opts, err := docopt.ParseArgs(usage, os.Args[1:], "0.0.1")
+	if err != nil {
+		log.LogFatal(log.Fields{}, "Unable to parse commandline args")
+	}
 
-	if err == nil {
-		fmt.Println("Arguments parsed successfully")
-		docopt.PrintHelpOnly(err, usage)
-		fmt.Println(opts)
-	} else {
-		fmt.Printf("%+v\n", err)
-		fmt.Println("Arguments NOT parsed successfully")
-		fmt.Println(err.Error())
-		fmt.Println(opts)
-	}
-	fmt.Println(opts)
 	if opts["--debug"].(bool) == true {
-		fmt.Println("\n\n***HELP**\n\n")
+		log.ConfigLogger(log.InfoLevel, os.Stdout)
+		log.LogInfo(log.Fields{}, "Sending DEBUG output to STDOUT")
 	}
+
+	log.LogInfo(log.Fields{"options": opts}, "Arguments parsed successfully")
+
 	// 	if len(os.Args) < 2 {
 	// 		fmt.Println("USAGE: ", os.Args[0], " <DATABASE>")
 	// 		os.Exit(1)
